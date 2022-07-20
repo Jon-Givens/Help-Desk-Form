bc0b60939bf75769e9b19005314ab0b58601556c
Jon-Givens <jon.givens@cdpipelinedevshop.com>
2022-07-20 16:20:30 +0000

    first commit

---
+++ help-desk-form/src/Components/SignIn.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import HelpIcon from '@mui/icons-material/Help';
+import { style } from '@mui/system';
+
+const descrptstyle = {
+    height: '100px'
+}
+const halfStyle = {
+    width: '10px'
+}
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <HelpIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Help Desk Form
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField 
+              InputProps={{ style: { width: 170 }}}
+              margin="normal"
+              required
+              id="First Name"
+              label="First Name"
+              name="First Name"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField 
+              InputProps={{ style: { width: 170 }}}
+              margin="normal"
+              required
+              id="Last Name"
+              label="Last Name"
+              name="Last Name"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              InputProps={{ style: { height: 150 }}}
+              margin="normal"
+              required
+              fullWidth
+              name="Description"
+              label="Description"
+              id="Description"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
